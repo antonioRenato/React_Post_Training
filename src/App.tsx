82f4b0ad4b0327080e6d73b3ea515d795b54ebdd
antonioRenato <antoniorpfs@gmail.com>
2024-01-14 14:37:36 -0300

    create header and sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
-function App() {
+import { Header } from "./components/Header/Header"
+
+import styles from './app.module.css'
+import './global.css'
+import { Sidebar } from "./components/Sidebar/Sidebar"
+
+export function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <span>Teste testando super testando</span>
+          <span>Teste testando super testando</span>
+        </main>
+      </div>
+    </div>    
   )
 }
-export default App
